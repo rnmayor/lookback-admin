ae116bd26afdc575c0292f659ffb42d2cb548c97
Ronel Mayor <ronelmayor0428@gmail.com>
2024-04-25 19:52:04 +0800

    refractor: Put AlertDialog as parent of cell-action component to fix auto-close when clicking dropdown-menu-item

--- components/pages/users/cell-action.tsx
+++ components/pages/users/cell-action.tsx
@@ -1,6 +1,16 @@
 "use client";
 
-import ConfirmModal from "@components/modals/confirm-modal";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@components/ui/alert-dialog";
 import { Button } from "@components/ui/button";
 import {
   DropdownMenu,
@@ -44,41 +54,56 @@ const CellAction = ({ data }: CellActionProps) => {
 
   return (
     <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="start">
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-          <Separator />
-          <DropdownMenuItem disabled={loading} className="px-0">
-            <Button
-              disabled={loading}
-              variant="ghost"
-              size="sm"
-              onClick={() => router.push(`/users/${data.id}`)}
-            >
-              <Edit className="mr-2 h-4 w-4" /> Update
+      <AlertDialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
             </Button>
-          </DropdownMenuItem>
-          {role === UserRole.SUPER_ADMIN && (
-            <DropdownMenuItem
-              disabled={loading}
-              className="px-0"
-              onSelect={(e) => e.preventDefault()}
-            >
-              <ConfirmModal onConfirm={onDelete}>
-                <Button disabled={loading} variant="ghost" size="sm">
-                  <Trash className="mr-2 h-4 w-4" /> Delete
-                </Button>
-              </ConfirmModal>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <Separator />
+            <DropdownMenuItem disabled={loading} className="px-0">
+              <Button
+                disabled={loading}
+                variant="ghost"
+                size="sm"
+                onClick={() => router.push(`/users/${data.id}`)}
+              >
+                <Edit className="mr-2 h-4 w-4" /> Update
+              </Button>
             </DropdownMenuItem>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
+            {role === UserRole.SUPER_ADMIN && (
+              <DropdownMenuItem disabled={loading} className="px-0">
+                <AlertDialogTrigger asChild>
+                  <Button disabled={loading} variant="ghost" size="sm">
+                    <Trash className="mr-2 h-4 w-4" /> Delete
+                  </Button>
+                </AlertDialogTrigger>
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={onDelete}
+                className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+              >
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </DropdownMenu>
+      </AlertDialog>
     </>
   );
 };
